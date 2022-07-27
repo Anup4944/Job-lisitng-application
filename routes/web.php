da0249c68177b03f5c2c382a4f86a46ea14db2bd
Anup <anuppoudel60@gmail.com>
2022-07-27 13:58:54 +1000

    middleware added

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,37 @@ Route::get("/", [ListingController::class, "index"]);
 
 // Show create forms
 
-Route::get("/listings/create", [ListingController::class, "create"]);
+Route::get("/listings/create", [
+    ListingController::class,
+    "create",
+])->middleware("auth");
 
 // Store Listing data
 
-Route::post("/listings", [ListingController::class, "store"]);
+Route::post("/listings", [ListingController::class, "store"])->middleware(
+    "auth"
+);
 
 // Show edit form
 
-Route::get("/listings/{listing}/edit", [ListingController::class, "edit"]);
+Route::get("/listings/{listing}/edit", [
+    ListingController::class,
+    "edit",
+])->middleware("auth");
 
 // Edit submit to Update
 
-Route::put("/listings/{listing}", [ListingController::class, "update"]);
+Route::put("/listings/{listing}", [
+    ListingController::class,
+    "update",
+])->middleware("auth");
 
 // Edit submit to Update
 
-Route::delete("/listings/{listing}", [ListingController::class, "destroy"]);
+Route::delete("/listings/{listing}", [
+    ListingController::class,
+    "destroy",
+])->middleware("auth");
 
 // Single Listing
 
@@ -35,7 +49,7 @@ Route::get("/listings/{listing}", [ListingController::class, "show"]);
 
 // Show register form
 
-Route::get("/register", [UserController::class, "create"]);
+Route::get("/register", [UserController::class, "create"])->middleware("guest");
 
 // Create new user
 
@@ -43,11 +57,13 @@ Route::post("/users", [UserController::class, "store"]);
 
 // Logout user
 
-Route::post("/logout", [UserController::class, "logout"]);
+Route::post("/logout", [UserController::class, "logout"])->middleware("auth");
 
 // Show login form
 
-Route::get("/login", [UserController::class, "login"]);
+Route::get("/login", [UserController::class, "login"])
+    ->name("login")
+    ->middleware("guest");
 
 // Login user
 
